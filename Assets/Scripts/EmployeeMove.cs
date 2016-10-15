271ed4b5fa4f469b33c02c3b5828ad46c75efd15
yhpham <yelenapham@gmail.com>
2016-10-15 09:48:13 -0400

    add employee movement

---
+++ Assets/Scripts/EmployeeMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmployeeMove : MonoBehaviour {
+    public float speed;
+    public float jumpVel;
+
+    public Vector3 vel;
+
+    public Rigidbody rigid;
+
+    void Awake() {
+        Cursor.visible = false;
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        vel = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+
+		if (GetArrowInput() && (vel != Vector3.zero)) {
+            transform.rotation = Quaternion.LookRotation(vel);
+        }
+        else {
+            rigid.angularVelocity = Vector3.zero;
+        }
+
+        rigid.velocity = vel;
+    }
+
+    bool GetArrowInput() {
+        return Input.GetKey(KeyCode.LeftArrow)
+            || Input.GetKey(KeyCode.RightArrow)
+            || Input.GetKey(KeyCode.UpArrow)
+            || Input.GetKey(KeyCode.DownArrow);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,7 @@ using System.Collections;
 
 enum Direction { Up, Down, Left, Right, };
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
     Rigidbody rigid;
 
     Vector3 movement;
@@ -18,34 +17,30 @@ public class PlayerMovement : MonoBehaviour
     float startTime;
     float journeyLength;
 
-    void Awake()
-    {
+    void Awake() {
         rigid = GetComponent<Rigidbody>();
     }
 		
-    void Update()
-    {
+    void Update() {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
         Move(h, v);
     }
 
-    void Move(float h, float v)
-    {
-        if (!moving && h != 0)
-        {
+    void Move(float h, float v) {
+        if (!moving && h != 0) {
             moving = true;
             if (h > 0)
                 nextDirection = Direction.Right;
             else
                 nextDirection = Direction.Left;
+            
             nextSpace = CenterVector(new Vector3(tPosition.x + h, tPosition.y, tPosition.z));
             startTime = Time.time;
             journeyLength = Vector3.Distance(tPosition, nextSpace);
         }
-        else if (!moving && v != 0)
-        {
+        else if (!moving && v != 0) {
             moving = true;
             if (v > 0)
                 nextDirection = Direction.Up;
@@ -56,10 +51,8 @@ public class PlayerMovement : MonoBehaviour
             journeyLength = Vector3.Distance(tPosition, nextSpace);
         }
 
-        if (moving)
-        {
-            switch (nextDirection)
-            {
+        if (moving) {
+            switch (nextDirection) {
                 case Direction.Up:
                     movement.Set(0f, 0f, 1);
                     break;
@@ -73,44 +66,38 @@ public class PlayerMovement : MonoBehaviour
                     movement.Set(1, 0f, 0f);
                     break;
             }
+            
             float distCovered = (Time.time - startTime) * speed;
             float fracJourney = distCovered / journeyLength;
             transform.position = Vector3.Lerp(tPosition, nextSpace, fracJourney);
         }
+        
         if (tPosition == nextSpace)
             moving = false;
     }
 
-    Vector3 CenterVector(Vector3 vec)
-    {
+    Vector3 CenterVector(Vector3 vec) {
         return new Vector3(RoundToNearestHalf(vec.x), vec.y, RoundToNearestHalf(vec.z));
     }
 
-    public static float RoundToNearestHalf(float num)
-    {
+    public static float RoundToNearestHalf(float num) {
         return Mathf.Round(num * 2f) / 2f;
     }
 
-    public Vector3 tPosition
-    {
-        get
-        {
+    public Vector3 tPosition {
+        get {
             return transform.position;
         }
-        set
-        {
+        set {
             transform.position = value;
         }
     }
 
-    public Vector3 rPosition
-    {
-        get
-        {
+    public Vector3 rPosition {
+        get {
             return rigid.position;
         }
-        set
-        {
+        set {
             rigid.position = value;
         }
     }
