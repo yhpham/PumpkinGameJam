f6e05b56c165e0d0aee5956ba195316ccdec6632
yhpham <yelenapham@gmail.com>
2016-10-15 11:24:32 -0400

    make player jump

--- Assets/Scripts/EmployeeMove.cs
+++ Assets/Scripts/EmployeeMove.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class EmployeeMove : MonoBehaviour {
     public float speed;
     public float jumpVel;
+    public float jumpDur;
+
+    public bool isGrounded = true;
+    public bool isJumping = false;
+
+    bool jumpClicked;
+    float jumpHeldForSeconds;
 
     public Vector3 vel;
 
@@ -14,14 +22,30 @@ public class EmployeeMove : MonoBehaviour {
         rigid = GetComponent<Rigidbody>();
     }
 
-    void Update() {
+    void FixedUpdate() {
         vel = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
 
-		if (GetArrowInput() && (vel != Vector3.zero)) {
+        if (GetArrowInput() && (vel != Vector3.zero)) {
             transform.rotation = Quaternion.LookRotation(vel);
         }
         else {
             rigid.angularVelocity = Vector3.zero;
+        }
+
+        jumpClicked = Input.GetKeyDown(KeyCode.Space);
+
+        if (jumpClicked && isGrounded) {
+            isGrounded = false;
+            isJumping = true;
+
+            rigid.AddForce(Vector3.up * jumpVel, ForceMode.VelocityChange);
+        }
+        else {
+            isGrounded = true;
+            isJumping = false;
+            jumpClicked = false;
+
+            vel.y = rigid.velocity.y;
         }
 
         rigid.velocity = vel;
