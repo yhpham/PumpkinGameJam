f9beccf702724ae06bfbea872ccd40240afa432d
Amy Liu <amyliu38@gmail.com>
2016-10-15 13:16:46 -0400

    Added gameover scenes and level endpoint

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.name == "PBlue") {
+			SceneManager.LoadScene ("BlueWins");
+		} else if (coll.gameObject.name == "PRed") {
+			SceneManager.LoadScene ("RedWins");
+		}
+	}
+}
