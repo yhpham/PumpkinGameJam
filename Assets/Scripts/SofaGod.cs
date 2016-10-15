18e00ffba8dec11a207ad7930fdc90c580a0db74
Amy Liu <amyliu38@gmail.com>
2016-10-14 22:38:24 -0400

    Sofa God - drop sofa and movement

--- Assets/Scripts/SofaGod.cs
+++ Assets/Scripts/SofaGod.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class SofaGod : MonoBehaviour {
 
 	public GameObject[] sofas;
+	public float moveSpeed = 1f;
 
 	private GameObject currentSofa;
 	private int nextSofaID;
 
-	private float cooldown = 3f;
+	private float cooldown = 2f;
 
 	private Rigidbody rb;
 
@@ -16,28 +17,37 @@ public class SofaGod : MonoBehaviour {
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 
-		currentSofa = GameObject.Instantiate(sofas[1]);
+		currentSofa = GameObject.Instantiate(sofas[0]);
+		currentSofa.transform.parent = this.transform;
 		currentSofa.transform.position = transform.position;
-		nextSofaID = Random.Range (0, 1);
+		nextSofaID = Random.Range (0, 2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.JoystickButton0) && cooldown < 0f) {
 			DropSofa ();
-			cooldown = 3f;
+			cooldown = 2f;
 		}
 		cooldown -= Time.deltaTime;
 
 
+		rb.velocity = new Vector3 (Input.GetAxis ("Horizontal2"), 0f, Input.GetAxis ("Vertical2")) * moveSpeed;
 
 	}
 
 	void DropSofa(){
-		Color color = currentSofa.GetComponent<Renderer> ().material.color;
-		color.a = 255f;
-		currentSofa.GetComponent<Renderer> ().material.color = color;
+		Renderer[] rends = currentSofa.GetComponentsInChildren<Renderer> ();
 
+		Color color = rends [0].material.color;
+		print (rends [0].material.color);
+		color.a = 1f;
+		print (color);
+
+		foreach (Renderer rend  in rends) {
+			rend.material.color = color;
+		}
+		currentSofa.transform.parent = null;
 		currentSofa.GetComponent<Rigidbody> ().useGravity = true;
 
 		Invoke("SetNextSofa", .5f);
@@ -47,7 +57,8 @@ public class SofaGod : MonoBehaviour {
 	void SetNextSofa(){
 		currentSofa = GameObject.Instantiate( sofas[nextSofaID] );
 		currentSofa.transform.position = transform.position;
+		currentSofa.transform.parent = this.transform;
 
-		nextSofaID = Random.Range (0, 1);
+		nextSofaID = Random.Range (0, 2);
 	}
 }
