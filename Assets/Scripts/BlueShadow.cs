19c25a7910b628d9a9bd7ed8d91117282fcecf6a
yhpham <yelenapham@gmail.com>
2016-10-15 16:29:11 -0400

    add player shadows

---
+++ Assets/Scripts/BlueShadow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueShadow : MonoBehaviour {
+
+	void Update () {
+		Vector3 pos = GameObject.Find("PBlue").transform.position;
+        transform.position = new Vector3(pos.x, transform.position.y, pos.z);
+	}
+}
---
+++ Assets/Scripts/RedShadow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedShadow : MonoBehaviour {
+
+	void Update () {
+		Vector3 pos = GameObject.Find("PRed").transform.position;
+        transform.position = new Vector3(pos.x, transform.position.y, pos.z);
+	}
+}
