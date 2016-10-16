532f5f6729db3991954b50f1918a375e5bd3b7f0
Chris Pike <chpike@umich.edu>
2016-10-16 00:16:28 -0400

    Start of power ups

--- Assets/Scripts/Employee.cs
+++ Assets/Scripts/Employee.cs
@@ -3,18 +3,6 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class Employee : MonoBehaviour {
-    bool _isDead = false;
-    public bool isDead {
-        get { return _isDead; }
-    }
-
-    int _points;
-    public int points {
-        get { return _points; }
-    }
-
-    float startEarning;
-
     public float speed;
     public float jumpVel;
 
@@ -29,6 +17,32 @@ public class Employee : MonoBehaviour {
 	public string vertical = "PBlueVertical";
 	public string jump = "PBlueJump";
 
+    bool _isDead = false;
+    public bool isDead {
+        get { return _isDead; }
+    }
+
+    int _points = 0;
+    public int points {
+        get { return _points; }
+    }
+
+    float startEarning;
+    const float earningInterval = 0.5f;
+
+    const int pointsForCouch = -20;
+    const int pointsForDying = -50;
+    const int pointsForLiving = 10;
+
+    const float powerUpDuration = 5.0f; 
+
+    bool invincible = false;
+    float invincibilityTimer = 0;
+
+    bool extraPoints = false;
+    float extraPointsTimer = 0;
+    const  int extraPointMultiplier = 2;
+
     void Awake() {
         Cursor.visible = false;
         rigid = GetComponent<Rigidbody>();
@@ -38,6 +52,7 @@ public class Employee : MonoBehaviour {
     void FixedUpdate() {
         Move();
         Score();
+        PowerUpTimers();
     }
 
     void Move() {
@@ -74,28 +89,63 @@ public class Employee : MonoBehaviour {
     }
 
 	void OnCollisionEnter(Collision col) {
+        if (invincible)
+            return;
+
 		if (col.gameObject.tag == "Floor") {
 			Die ();
 		}
         else if(col.gameObject.tag != gameObject.tag) {
-            _points -= 20;
+            _points += pointsForCouch;
         }
-	}
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if (invincible || extraPoints)
+            return;
+
+        if (col.gameObject.tag == "Invincible") {
+            invincible = true;
+        }
+        else if (col.gameObject.tag == "ExtraPoints") {
+            extraPoints = true;
+        }
+    }
+
+    void PowerUpTimers() {
+        if(invincible) {
+            invincibilityTimer += Time.deltaTime;
+            if(invincibilityTimer > powerUpDuration) {
+                invincible = false;
+                invincibilityTimer = 0;
+            }
+        }
+        if (extraPoints) {
+            extraPointsTimer += Time.deltaTime;
+            if (extraPointsTimer > powerUpDuration) {
+                extraPoints = false;
+                extraPointsTimer = 0;
+            }
+        }
+    }
 
     void Score() {
         if (_isDead)
             return;
 
         startEarning += Time.deltaTime;
-        if(startEarning > 0.5f) {
-            _points += 10;
+        if(startEarning > earningInterval) {
+            if (extraPoints)
+                _points += pointsForLiving * extraPointMultiplier;
+            else
+                _points += pointsForLiving;
             startEarning = 0;
         }
     }
 
     public void Die() {
         _isDead = true;
-        _points -= 50;
+        _points += pointsForDying;
         startEarning = 0;
     }
 
