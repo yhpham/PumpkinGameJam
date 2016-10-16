114011f2bdc3a9af118c2ae7d7b2e5c158351dc3
Amy Liu <amyliu38@gmail.com>
2016-10-15 21:24:14 -0400

    Player shadow bug fix

--- Assets/Scripts/BlueShadow.cs
+++ Assets/Scripts/BlueShadow.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class BlueShadow : MonoBehaviour {
 
+	RaycastHit hit;
+
 	void Update () {
 		Vector3 pos = GameObject.Find("PBlue").transform.position;
-        transform.position = new Vector3(pos.x, transform.position.y, pos.z);
+
+		if (Physics.Raycast(pos, -Vector3.up, out hit)) {
+			transform.position = new Vector3(pos.x, hit.point.y, pos.z);
+		}
+		else {
+			transform.position = new Vector3(pos.x, transform.position.y, pos.z);
+		}
 	}
 }
