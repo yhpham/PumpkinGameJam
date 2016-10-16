9e77ae0b45ecb69d5865bead713f55805f6d4b4e
yhpham <yelenapham@gmail.com>
2016-10-15 20:41:30 -0400

    add start menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+	//public GameObject instructions;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame(){
+		SceneManager.LoadScene("scene_lava");
+	}
+
+	public void Quit(){
+		Application.Quit();
+	}
+
+	public void Instructions(){
+		//instructions.enabled = true;
+		return;
+	}
+
+}
